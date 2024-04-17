fa99c230cfc01815d6635ac92e292d87eff19bec
Lucas Chaves Meyles <lchavesmeyles@gmail.com>
2024-04-17 16:21:44 -0700

    Finished mini message board

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', allMessages: messages });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res, next) => {
+  messages.push({ text: req.body.message, user: req.body.user, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
